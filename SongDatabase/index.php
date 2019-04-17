078f4fff04bd7662c59e05324a96fb0d25bb3810
josephmichaud <39277336+josephmichaud@users.noreply.github.com>
2019-04-17 16:25:44 -0400

    Update index.php

--- SongDatabase/index.php
+++ SongDatabase/index.php
@@ -1,27 +1,6 @@
 <?php
     require_once './php/db_connect.php';
 ?>
-<?php 
-if(isset($_POST['gender']) && !empty($_POST['name'])) {
-    $gender = $_POST['gender'];
-	$name = strtoupper(trim($_POST['name']));	
-    $selectStmt = "SELECT COUNT(*) AS matches FROM `BABYNAMEVOTES` WHERE Name='" . $name . "' AND Gender='" . $gender . "';";
-    $result = $db->query($selectStmt);
-    $matches = ($result->fetch_assoc())["matches"];
-    if($matches == 1) {
-        $updateStmt = "UPDATE `BABYNAMEVOTES` SET Votes = Votes + 1 WHERE Name='" . $name . "' AND Gender='" . $gender . "';";
-        $db->query($updateStmt);
-    } elseif($matches == 0) {
-        $insertStmt = "INSERT INTO `BABYNAMEVOTES` VALUES ('" . $name . "','" . $gender . "',1);";
-        $db->query($insertStmt);
-    }
-}
-if(isset($_POST['table'])) {
-    $table = $_POST['table'];
-} else {
-    $table = "Songs";
-}
-?>
 <html>
     <head>
         <title>Music Database</title>
@@ -40,47 +19,23 @@ if(isset($_POST['table'])) {
             <h6>Music Database</h6>
             <div class="clearfix"> </div>
         </div>
-        <!--
-        <div class="header" id="home">
-        <div class="top_spl_courses">
-            <div class="container">
-                <h3 class="headerw3">Submit a query:</h3>
-                    <div class="text-info">
-                        <form action="" method="post">
-                            <div class="form-group">
-                                <textarea class=".form-control" rows="4" cols="50" placeholder="Enter query here."></textarea>
-                                <input type="text" class="form-control" name="query" id="query">
-                                <span id="tempglyph" class=""></span>
-                            </div>
-                            &nbsp;&nbsp;<button type="submit" name="submit" id="submit" class="btn btn-default">Vote</button>
-                            <br>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
--->
-        
         <div class="container">
-            <h2>SELECT * FROM  <form action="" method="post"> <select name="table" id="table" onchange="this.form.submit()"><option value="" selected disabled hidden>Choose here</option><option value="Songs">Songs</option><option value="Artists">Artists</option><option value="Albums">Albums</option><option value="Contracts">Contracts</option></select></form></h2>           
-            <h2><?php echo $table ?></h2>
+            <h2>Artists of Top 3 Songs</h2>           
             <table class="table table-condensed">
                 <thead>
                     <tr>
-                        <?php
-                            $selectStmt = "SHOW COLUMNS FROM " . $table .";";
-                            $result = $db->query($selectStmt);
-                            if($result->num_rows > 0) {
-                                while($row = $result->fetch_assoc()) {
-                                    echo "<th>" . $row['Field'] . "</th>";
-                                }
-                            }
-                        ?>
+                        <th>Name</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                        $selectStmt = "SELECT * FROM " . $table . ";";
+                        $selectStmt = 
+                            "SELECT DISTINCT A.name
+                            FROM Artists A, Albums Al, Songs S
+                            WHERE A.a_id = Al.a_id 
+                            AND S.s_id = Al.s_id
+                            ORDER BY S.hits DESC
+                            LIMIT 3;";
                         $result = $db->query($selectStmt);
                         if($result->num_rows > 0) {
                             while($row = $result->fetch_assoc()) {
@@ -93,7 +48,160 @@ if(isset($_POST['table'])) {
                         }
                     ?>
                 </tbody>
-              </table>
+            </table>
+        </div>
+        <br>
+        <div class="container">
+            <h2>Songs with Fewer Hits than the Average</h2>           
+            <table class="table table-condensed">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Hits</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $selectStmt = 
+                            "SELECT S1.name, S1.hits 
+                            FROM Songs S1
+                            WHERE S1.hits < (SELECT AVG(S2.hits)
+                                            FROM Songs S2);";
+                        $result = $db->query($selectStmt);
+                        if($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                echo "<tr>";
+                                foreach ($row as $value) {
+                                    echo "<td>" . $value . "</td>";
+                                }
+                                echo "</tr>";
+                            }
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        <br>
+        <div class="container">
+            <h2>Record Labels That Made Songs With a Million Hits</h2>           
+            <table class="table table-condensed">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $selectStmt = 
+                            "SELECT DISTINCT R.name
+                            FROM Record_Label R, Contracts C, Artists A, Albums Al, Songs S
+                            WHERE R.r_id = C.r_id 
+                            AND C.a_id = A.a_id 
+                            AND A.a_id = Al.a_id 
+                            AND Al.s_id = S.s_id 
+                            AND S.hits > 1000000;";
+                        $result = $db->query($selectStmt);
+                        if($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                echo "<tr>";
+                                foreach ($row as $value) {
+                                    echo "<td>" . $value . "</td>";
+                                }
+                                echo "</tr>";
+                            }
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        <br>
+        <div class="container">
+            <h2>Number of RnB Songs</h2>           
+            <table class="table table-condensed">
+                <thead>
+                    <tr>
+                        <th>Count</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $selectStmt = 
+                            'SELECT COUNT(*)
+                            FROM Songs S
+                            WHERE S.genre = "RnB";';
+                        $result = $db->query($selectStmt);
+                        if($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                echo "<tr>";
+                                foreach ($row as $value) {
+                                    echo "<td>" . $value . "</td>";
+                                }
+                                echo "</tr>";
+                            }
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        <br>
+        <div class="container">
+            <h2>Artists with Albums Published Before 2000</h2>           
+            <table class="table table-condensed">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $selectStmt = 
+                            "SELECT DISTINCT A.name
+                            FROM Artists A, Albums Al
+                            WHERE A.a_id = Al.a_id
+                            AND Al.year < 2000;";
+                        $result = $db->query($selectStmt);
+                        if($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                echo "<tr>";
+                                foreach ($row as $value) {
+                                    echo "<td>" . $value . "</td>";
+                                }
+                                echo "</tr>";
+                            }
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        <br>
+        <div class="container">
+            <h2>Album Names of Artists That Start with “M”</h2>           
+            <table class="table table-condensed">
+                <thead>
+                    <tr>
+                        <th>Album Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $selectStmt = 
+                            'SELECT DISTINCT Al.name
+                            FROM Artists A, Albums Al
+                            WHERE A.a_id = Al.a_id
+                            AND A.name LIKE "M%";';
+                        $result = $db->query($selectStmt);
+                        if($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                echo "<tr>";
+                                foreach ($row as $value) {
+                                    echo "<td>" . $value . "</td>";
+                                }
+                                echo "</tr>";
+                            }
+                        }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </body> 
     <div class="insert-post-ads1" style="margin-top:20px;"></div>
